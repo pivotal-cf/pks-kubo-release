76caad618b821ebc40071d51320dbb639c690581
Daniel Keeney <dkeeney@pivotal.io>
2019-11-08 16:26:04 -0800

    Add command line flags
    Authored-by: Daniel Keeney <dkeeney@pivotal.io>

--- src/osl-collector/main.go
+++ src/osl-collector/main.go
@@ -1,10 +1,28 @@
 package main
 
 import (
-	"os"
+	"flag"
 	jsonReader "osl-collector/jsonReader"
 )
 
+type OslCollectorInputs struct {
+	Folder string
+	JsonFileName string
+}
+
 func main() {
-	jsonReader.MyFunction(os.Args[1], os.Args[2])
+	inputs := gatherFlags()
+	jsonReader.MyFunction(inputs.Folder, inputs.JsonFileName)
+}
+
+func gatherFlags() OslCollectorInputs {
+	folderPtr := flag.String("folder", jsonReader.DefaultFolder, "Folder to search for sub directories")
+	jsonFileNamePtr := flag.String("jsonFileName", jsonReader.DefaultJsonFile, "File name to search for")
+
+	flag.Parse()
+
+	return OslCollectorInputs{
+		Folder:       *folderPtr,
+		JsonFileName: *jsonFileNamePtr,
+	}
 }
