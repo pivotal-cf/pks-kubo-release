7b9108ec402fc4d7e62c7632ead891a9de23dcf6
Daniel Keeney <dkeeney@pivotal.io>
2019-11-13 10:25:49 -0800

    Add test for symlinks
    Authored-by: Daniel Keeney <dkeeney@pivotal.io>

--- src/osl-collector/core/fileFinder_test.go
+++ src/osl-collector/core/fileFinder_test.go
@@ -1,7 +1,9 @@
 package core_test
 
 import (
+	"os"
 	jsonReader "osl-collector/core"
+	"path"
 	"testing"
 )
 
@@ -16,4 +18,28 @@ func TestFindFiles(t *testing.T) {
 		t.Errorf("Should have found %s but found %s\n", expected, jsonFiles[0])
 	}
 
+}
+
+func TestFindFiles_Symlink(t *testing.T) {
+	symlinkPath := "symLinkToTestDir2"
+	err := os.Symlink("testNested/testDir2/", symlinkPath)
+	if err != nil {
+		t.Error(err)
+	}
+	defer func() {
+		err := os.Remove(symlinkPath)
+		if err != nil {
+			t.Error(err)
+		}
+	}()
+
+	jsonFiles := jsonReader.FindFiles(jsonReader.DefaultFolder, jsonReader.DefaultOslPackageFile)
+
+	if len(jsonFiles) != 1 {
+		t.Errorf("Should have found 1 test json file but found %d", len(jsonFiles))
+	}
+	expected := path.Join(symlinkPath, jsonReader.DefaultOslPackageFile)
+	if jsonFiles[0] != expected {
+		t.Errorf("Should have found %s but found %s\n", expected, jsonFiles[0])
+	}
 }
