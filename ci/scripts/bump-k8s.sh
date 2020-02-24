c8cdf21ca4cab608307c64f1b9173a47456e5899
Daniel Keeney <dkeeney@pivotal.io>
2020-02-24 15:18:36 -0800

    Adding base branch support for pr
    [#171283538]

--- ci/scripts/bump-k8s.sh
+++ ci/scripts/bump-k8s.sh
@@ -7,6 +7,7 @@ pr_release() {
   version="$1"
   git_repo="$2"
   script_name="$3"
+  base_branch="$4"
 
   concourse_base_name="git-${git_repo}"
 
@@ -23,7 +24,7 @@ blobstore:
     secret_access_key: ${SECRET_ACCESS_KEY}
 EOF
     bosh upload-blobs
-    generate_pull_request "kubernetes" "$version" "${git_repo}" "master"
+    generate_pull_request "kubernetes" "$version" "${git_repo}" "${base_branch}"
   else
     echo "Kubernetes version is already up-to-date"
   fi
@@ -31,18 +32,23 @@ EOF
   popd
 }
 
-if [ "${REPO:-}" == "windows" ]; then
-  git_repo="pks-kubo-release-windows"
-  script_name="download_k8s_binaries"
-else
-  git_repo="pks-kubo-release"
-  if [ "false" == "$USE_COMMON_CORE" ]; then
-    script_name="download_k8s_binaries_google"
-  else
-    script_name="download_k8s_binaries_common_core"
-  fi
-fi
+main() {
 
-# BINARY_DIRECTORY should be declared in the pipeline via params
-version=$(cat "$PWD/$BINARY_DIRECTORY/version")
-pr_release "$version" "$git_repo" "$script_name"
+  if [ "${REPO:-}" == "windows" ]; then
+    git_repo="pks-kubo-release-windows"
+    script_name="download_k8s_binaries"
+  else
+    git_repo="pks-kubo-release"
+    if [ "false" == "$USE_COMMON_CORE" ]; then
+      script_name="download_k8s_binaries_google"
+    else
+      script_name="download_k8s_binaries_common_core"
+    fi
+  fi
+
+  # BINARY_DIRECTORY should be declared in the pipeline via params
+  version=$(cat "$PWD/$BINARY_DIRECTORY/version")
+  pr_release "$version" "$git_repo" "$script_name" "${BASE_BRANCH}"
+}
+
+main $@
