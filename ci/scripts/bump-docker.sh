a01f80d8d9e184232de66e8013542b164d33ecbb
Daniel Keeney <dkeeney@pivotal.io>
2020-03-09 16:37:02 -0700

    Infra for downloading docker binaries for linux
    [#171338582]
    
    Co-authored-by: Daniel Keeney <dkeeney@pivotal.io>
    Co-authored-by: Shimon Walner <swalner@pivotal.io>

---
+++ ci/scripts/bump-docker.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+set -exu -o pipefail
+
+source git-pks-kubo-release-ci/ci/scripts/lib/generate-pr.sh
+
+pr_release() {
+  version="$1"
+  git_repo="$2"
+  script_name="$3"
+  base_branch="$4"
+
+  concourse_base_name="git-${git_repo}"
+
+  cp -r "${concourse_base_name}/." "${concourse_base_name}-output"
+  pushd "${concourse_base_name}-output"
+
+  ../git-pks-kubo-release-ci/ci/scripts/$script_name $version $(pwd)
+
+  if [ -n "$(git status --porcelain)" ]; then
+    cat <<EOF > "config/private.yml"
+blobstore:
+  options:
+    access_key_id: ${ACCESS_KEY_ID}
+    secret_access_key: ${SECRET_ACCESS_KEY}
+EOF
+    bosh upload-blobs
+    generate_pull_request "kubernetes" "$version" "${git_repo}" "${base_branch}"
+  else
+    echo "Kubernetes version is already up-to-date"
+  fi
+
+  popd
+}
+
+main() {
+
+  if [ "${REPO:-}" == "windows" ]; then
+    # TODO: ensure these are correct
+    git_repo="pks-kubo-release-windows"
+    script_name="download_docker_binaries_windows.sh"
+  else
+    git_repo="pks-docker-boshrelease"
+    script_name="download_docker_binaries_linux.sh"
+  fi
+
+  # BINARY_DIRECTORY should be declared in the pipeline via params
+  version=$(cat "$PWD/$BINARY_DIRECTORY/version")
+  pr_release "$version" "$git_repo" "$script_name" "${BASE_BRANCH}"
+}
+
+main $@
---
+++ ci/scripts/download_docker_binaries_linux.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+set -ex
+
+
+main() {
+  local staging_dir docker_version release_dir
+
+  if [ $# -lt 2 ]; then
+    echo "Usage: $(basename "$0") <DOCKER_VERSION> <RELEASE_DIR>"
+    exit  1
+  fi
+
+  docker_version=$1
+  release_dir=$2
+
+  trap '{ rm -rf "$staging_dir"; }' EXIT
+
+  pushd "$release_dir" || exit 1
+
+    existing_docker_spec=$(bosh blobs --column path | grep "docker-" | grep -o -E "docker-([0-9]+\.)+[0-9]+")
+    existing_docker_version=$(echo "$existing_docker_spec" | grep -o -E '([0-9]+\.)+[0-9]+')
+
+    if [ "$existing_docker_version" == "$docker_version" ]; then
+        echo "Docker version already up-to-date."
+    else
+      pushd "packages/docker" || exit 1
+        # change version number found in string like DOCKER_VERSION="1.16.3"
+        sed -E -i -e "s/DOCKER_VERSION=\"([0-9]+\.)+[0-9]+\"/DOCKER_VERSION=\"${docker_version}\"/" packaging
+        sed -E -i -e "s/${existing_docker_spec}/docker-${docker_version}/" spec
+      popd || exit 1
+
+      wget https://download.docker.com/linux/static/stable/x86_64/docker-${docker_version}.tgz -O docker-${docker_version}.tgz
+
+      add_blob "docker-${docker_version}.tgz" "${docker_version}"
+    fi
+  popd || exit 1
+}
+
+add_blob() {
+  local binary_name blob_name docker_version
+  binary_name="$1"
+  docker_version="$2"
+  blob_name=$(bosh blobs --column path | grep "${binary_name}\s$" | xargs)
+
+  bosh remove-blob "$blob_name"
+
+  bosh add-blob "docker-${docker_version}.tgz" "docker/$binary_name"
+}
+
+main "$@"
