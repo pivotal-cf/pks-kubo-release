664c1707b062b90282861c5e0e47a8da5f01874b
Todd Sedano <tsedano@pivotal.io>
2020-03-31 17:27:59 -0700

    WIP: added config and scripts to upload release

---
+++ ci/utils/upload-kubo-release.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+
+source git-boshcycle-ci/ci/utils/use-bosh.sh
+
+pushd git-pks-kubo-release
+  RELEASE_TARBALL=$(ls kubo-release-*.tgz)
+  bosh upload-release ${RELEASE_TARBALL}
+popd
