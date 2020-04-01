cdb98f42ebea8ff7e5ce7cadc042ba2e67a9c5c2
Todd Sedano <tsedano@pivotal.io>
2020-03-31 17:02:18 -0700

    WIP: get kubo and kubo-windows versions from tarball

--- ci/open-source-management/deploy-manifest.sh
+++ ci/open-source-management/deploy-manifest.sh
@@ -3,9 +3,10 @@
 set -euxo pipefail
 
 source git-boshcycle-ci/ci/utils/use-bosh.sh
-source git-boshcycle-ci/ci/utils/kubo-sha.sh
-source git-boshcycle-ci/ci/utils/kubo-windows-sha.sh
 source git-boshcycle-ci/ci/open-source-management/deployment-name.sh
+
+PKS_KUBO_VERSION=$(cat gcs-pks-kubo-release/version)
+PKS_KUBO_WINDOWS_VERSION=$(cat gcs-pks-kubo-release-windows/version)
 
 export NETWORK_NAME=`bosh int <(bosh cloud-config) --path /networks | yq -r '.[] | select(.name|test(".*pks-services-subnet.*")) | .name'`
 export FIRST_AZ=`bosh int <(bosh cloud-config) --path /networks | yq -r '.[] | select(.name|test(".*pks-services-subnet.*")) | .subnets[0].azs[0]'`
@@ -14,8 +15,8 @@ bosh deploy \
   --non-interactive \
   --deployment="${DEPLOYMENT_NAME}" \
   --var=deployment-name="${DEPLOYMENT_NAME}" \
-  --var=kubo-version="${KUBO_GIT_SHA}" \
-  --var=kubo-windows-version="${KUBO_WINDOWS_GIT_SHA}" \
+  --var=kubo-version="${PKS_KUBO_VERSION}" \
+  --var=kubo-windows-version="${PKS_KUBO_WINDOWS_VERSION}" \
   --var=network-name="${NETWORK_NAME}" \
   --var=first-az="${FIRST_AZ}" \
   git-boshcycle-ci/ci/open-source-management/open-source-management-manifest.yml
