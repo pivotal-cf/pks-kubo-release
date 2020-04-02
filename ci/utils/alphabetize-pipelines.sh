237bee1b0fca41dff301b2c7a1becfdf2afe9269
Todd Sedano <tsedano@pivotal.io>
2020-04-02 09:03:53 -0700

    WIP: implement array splitting

--- ci/utils/alphabetize-pipelines.sh
+++ ci/utils/alphabetize-pipelines.sh
@@ -5,19 +5,19 @@ set -euxo pipefail
 # TODO: make Concourse team a parameter
 CONCOURSE_TEAM="pks-bosh-lifecycle"
 
-raw_pipeline_data=$(fly -t ${CONCOURSE_TEAM} pipelines)
+raw_pipeline_data=$(fly -t ${CONCOURSE_TEAM} pipelines | sort)
 
-# TODO: split the raw output into an array of output lines (pipeline rows)
-#  pipeline_rows <= raw_pipeline_data
-# TODO: alphabetize pipeline_rows
+#  Split raw_pipeline_data into an array
+pipeline_rows=(); while read -r line; do pipeline_rows+=("$line"); done <<<"$raw_pipeline_data"; declare -p pipeline_rows;
 
 order_pipelines_command="fly -t ${CONCOURSE_TEAM} order-pipelines "
 for pipeline_row in "${pipeline_rows[@]}"
 do
-  # TODO: parse the pipeline row to get the pipeline name
-  # pipeline <= pipeline_row
+  # Split row into pieces by space and take first element as pipeline name
+  set $pipeline_row
+  pipeline=$1
+  # Append pipeline to ordering command
   order_pipelines_command="${order_pipelines_command} --pipeline ${pipeline} "
 done
 
-# TODO: run the ordering command
-echo "order_pipelines_command=[${order_pipelines_command}]"
+${order_pipelines_command}
