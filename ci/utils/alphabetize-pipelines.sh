98006d155958d37565a567fd47fec14fcd03b450
Todd Sedano <tsedano@pivotal.io>
2020-04-02 08:32:15 -0700

    WIP: comments

--- ci/utils/alphabetize-pipelines.sh
+++ ci/utils/alphabetize-pipelines.sh
@@ -2,15 +2,16 @@
 
 set -euxo pipefail
 
-# TODO: make team a parameter
-raw_pipeline_data=$(fly -t pks-bosh-lifecycle pipelines)
+# TODO: make Concourse team a parameter
+CONCOURSE_TEAM="pks-bosh-lifecycle"
+
+raw_pipeline_data=$(fly -t ${CONCOURSE_TEAM} pipelines)
 
 # TODO: split the raw output into an array of output lines (pipeline rows)
-#  pipeline_rows=raw_pipeline_data.split()
-# TODO: alphabetize
+#  pipeline_rows <= raw_pipeline_data
+# TODO: alphabetize pipeline_rows
 
-order_pipelines_command="fly -t pks-bosh-lifecycle order-pipelines "
-## now loop through the pipeline rows array
+order_pipelines_command="fly -t ${CONCOURSE_TEAM} order-pipelines "
 for pipeline_row in "${pipeline_rows[@]}"
 do
   # TODO: parse the pipeline row to get the pipeline name
@@ -18,5 +19,5 @@ do
   order_pipelines_command="${order_pipelines_command} --pipeline ${pipeline} "
 done
 
-# TODO: run the command
+# TODO: run the ordering command
 echo "order_pipelines_command=[${order_pipelines_command}]"
