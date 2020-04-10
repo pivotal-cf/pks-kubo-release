800ff46d94a6e82e8af44af9eed54eefc27d669e
Larry Hamel <lhamel@pivotal.io>
2020-04-09 17:05:58 -0700

    Add additional etcd security tests for v3
    Co-authored-by: Larry Hamel <lhamel@pivotal.io>
    Co-authored-by: Daniel Keeney <dkeeney@pivotal.io>

--- src/tests/security-tests/etcd/etcd_worker_cert_permission_test.go
+++ src/tests/security-tests/etcd/etcd_worker_cert_permission_test.go
@@ -8,70 +8,138 @@ import (
 	. "github.com/onsi/gomega"
 )
 
+const v2PermissionsErrorMessage = "Insufficient credentials"
+const v3PermissionsErrorMessage = "etcdserver: permission denied"
+
 var _ = Describe("Etcd cert on worker", func() {
 	var (
 		directory string
 	)
+	Context("for v2", func() {
+		Context("For directorys under /coreos.com/network/", func() {
+			BeforeEach(func() {
+				directory = "/coreos.com/network/"
+			})
 
-	Context("For directories under /coreos.com/network/", func() {
-		BeforeEach(func() {
-			directory = "/coreos.com/network/"
+			AfterEach(func() {
+				for _, vm := range workers {
+					args := []string{"rm", fmt.Sprintf("%s%s", directory, vm.ID)}
+					value := test_helpers.RunEtcdCommandFromMasterWithFullPrivilege(2, deploymentName, master.ID, args...)
+					Expect(value).NotTo(ContainSubstring(v2PermissionsErrorMessage))
+				}
+			})
+
+			It("should have read access ", func() {
+				args := []string{"ls", directory}
+				for _, vm := range workers {
+					value := test_helpers.RunEtcdCommandFromWorker(2, deploymentName, vm.ID, args...)
+					Expect(value).NotTo(ContainSubstring(v2PermissionsErrorMessage))
+				}
+
+			})
+
+			It("should have write access", func() {
+				for _, vm := range workers {
+					args := []string{"set", fmt.Sprintf("%s%s", directory, vm.ID), vm.ID}
+					value := test_helpers.RunEtcdCommandFromWorker(2, deploymentName, vm.ID, args...)
+					Expect(value).NotTo(ContainSubstring(v2PermissionsErrorMessage))
+				}
+
+			})
 		})
 
-		AfterEach(func() {
-			for _, vm := range workers {
-				args := []string{"del", fmt.Sprintf("%s%s", directory, vm.ID)}
-				value := test_helpers.RunEtcdCommandFromMasterWithFullPrivilege(deploymentName, master.ID, args...)
-				Expect(value).NotTo(ContainSubstring("Insufficient credentials"))
-			}
-		})
+		Context("For directorys under /", func() {
+			BeforeEach(func() {
+				directory = "/"
+			})
 
-		It("should have read access ", func() {
-			args := []string{"get", "--prefix", directory}
-			for _, vm := range workers {
-				value := test_helpers.RunEtcdCommandFromWorker(deploymentName, vm.ID, args...)
-				Expect(value).NotTo(ContainSubstring("Insufficient credentials"))
-			}
+			AfterEach(func() {
+				for _, vm := range workers {
+					args := []string{"rm", fmt.Sprintf("%s%s", directory, vm.ID)}
+					value := test_helpers.RunEtcdCommandFromMasterWithFullPrivilege(2, deploymentName, master.ID, args...)
+					Expect(value).NotTo(ContainSubstring(v2PermissionsErrorMessage))
+				}
+			})
 
-		})
+			It("should not have read access", func() {
+				for _, vm := range workers {
+					args := []string{"ls", directory}
+					value := test_helpers.RunEtcdCommandFromWorker(2, deploymentName, vm.ID, args...)
+					Expect(value).To(ContainSubstring(v2PermissionsErrorMessage))
+				}
+			})
 
-		It("should have write access", func() {
-			for _, vm := range workers {
-				args := []string{"put", fmt.Sprintf("%s%s", directory, vm.ID), vm.ID}
-				value := test_helpers.RunEtcdCommandFromWorker(deploymentName, vm.ID, args...)
-				Expect(value).NotTo(ContainSubstring("Insufficient credentials"))
-			}
-
+			It("should not have write access", func() {
+				for _, vm := range workers {
+					args := []string{"set", fmt.Sprintf("%s%s", directory, vm.ID), vm.ID}
+					value := test_helpers.RunEtcdCommandFromWorker(2, deploymentName, vm.ID, args...)
+					Expect(value).To(ContainSubstring(v2PermissionsErrorMessage))
+				}
+			})
 		})
 	})
 
-	Context("For directories under /", func() {
-		BeforeEach(func() {
-			directory = "/"
-		})
+	Context("for v3", func() {
+		Context("For directories under /coreos.com/network/", func() {
+			BeforeEach(func() {
+				directory = "/coreos.com/network/"
+			})
 
-		AfterEach(func() {
-			for _, vm := range workers {
-				args := []string{"del", fmt.Sprintf("%s%s", directory, vm.ID)}
-				value := test_helpers.RunEtcdCommandFromMasterWithFullPrivilege(deploymentName, master.ID, args...)
-				Expect(value).NotTo(ContainSubstring("Insufficient credentials"))
-			}
-		})
+			AfterEach(func() {
+				for _, vm := range workers {
+					args := []string{"del", fmt.Sprintf("%s%s", directory, vm.ID)}
+					value := test_helpers.RunEtcdCommandFromMasterWithFullPrivilege(3, deploymentName, master.ID, args...)
+					Expect(value).NotTo(ContainSubstring(v3PermissionsErrorMessage))
+				}
+			})
 
-		It("should not have read access", func() {
-			for _, vm := range workers {
+			It("should have read access ", func() {
 				args := []string{"get", "--prefix", directory}
-				value := test_helpers.RunEtcdCommandFromWorker(deploymentName, vm.ID, args...)
-				Expect(value).To(ContainSubstring("Insufficient credentials"))
-			}
+				for _, vm := range workers {
+					value := test_helpers.RunEtcdCommandFromWorker(3, deploymentName, vm.ID, args...)
+					Expect(value).NotTo(ContainSubstring(v3PermissionsErrorMessage))
+				}
+
+			})
+
+			It("should have write access", func() {
+				for _, vm := range workers {
+					args := []string{"put", fmt.Sprintf("%s%s", directory, vm.ID), vm.ID}
+					value := test_helpers.RunEtcdCommandFromWorker(3, deploymentName, vm.ID, args...)
+					Expect(value).NotTo(ContainSubstring(v3PermissionsErrorMessage))
+				}
+
+			})
 		})
 
-		It("should not have write access", func() {
-			for _, vm := range workers {
-				args := []string{"put", fmt.Sprintf("%s%s", directory, vm.ID), vm.ID}
-				value := test_helpers.RunEtcdCommandFromWorker(deploymentName, vm.ID, args...)
-				Expect(value).To(ContainSubstring("Insufficient credentials"))
-			}
+		Context("For directories under /", func() {
+			BeforeEach(func() {
+				directory = "/"
+			})
+
+			AfterEach(func() {
+				for _, vm := range workers {
+					args := []string{"del", fmt.Sprintf("%s%s", directory, vm.ID)}
+					value := test_helpers.RunEtcdCommandFromMasterWithFullPrivilege(3, deploymentName, master.ID, args...)
+					Expect(value).NotTo(ContainSubstring(v3PermissionsErrorMessage))
+				}
+			})
+
+			It("should not have read access", func() {
+				for _, vm := range workers {
+					args := []string{"get", "--prefix", directory}
+					value := test_helpers.RunEtcdCommandFromWorker(3, deploymentName, vm.ID, args...)
+					Expect(value).To(ContainSubstring(v3PermissionsErrorMessage))
+				}
+			})
+
+			It("should not have write access", func() {
+				for _, vm := range workers {
+					args := []string{"put", fmt.Sprintf("%s%s", directory, vm.ID), vm.ID}
+					value := test_helpers.RunEtcdCommandFromWorker(3, deploymentName, vm.ID, args...)
+					Expect(value).To(ContainSubstring(v3PermissionsErrorMessage))
+				}
+			})
 		})
 	})
 })
--- src/tests/test_helpers/bosh_runner.go
+++ src/tests/test_helpers/bosh_runner.go
@@ -12,15 +12,16 @@ import (
 
 const EtcdHostname = "master-0.etcd.cfcr.internal"
 
-func RunEtcdCommandFromWorker(deployment, workerID string, args ...string) string {
+func RunEtcdCommandFromWorker(etcdVersion int, deployment, workerID string, args ...string) string {
 	// to test manually, this is the combined base command:
 	// /var/vcap/packages/etcdctl/etcdctl --endpoints https://master-0.etcd.cfcr.internal:2379 --cert /var/vcap/jobs/flanneld/config/etcd-client.crt --key /var/vcap/jobs/flanneld/config/etcd-client.key --cacert /var/vcap/jobs/flanneld/config/etcd-ca.crt
 	remoteArgs := []string{
+		fmt.Sprintf("ETCDCTL_API=%d", etcdVersion),
 		"/var/vcap/packages/etcdctl/etcdctl",
 		fmt.Sprintf("--endpoints https://%s:2379", EtcdHostname),
-		"--cert", "/var/vcap/jobs/flanneld/config/etcd-client.crt",
-		"--key", "/var/vcap/jobs/flanneld/config/etcd-client.key",
-		"--cacert", "/var/vcap/jobs/flanneld/config/etcd-ca.crt",
+		getCertFlag(etcdVersion), "/var/vcap/jobs/flanneld/config/etcd-client.crt",
+		getKeyFlag(etcdVersion), "/var/vcap/jobs/flanneld/config/etcd-client.key",
+		getCacertFlag(etcdVersion), "/var/vcap/jobs/flanneld/config/etcd-ca.crt",
 	}
 	remoteArgs = append(remoteArgs, args...)
 	s := RunSSHWithDeployment(deployment, "worker/"+workerID, fmt.Sprintf("sudo su -c '%s'", strings.Join(remoteArgs, " ")))
@@ -29,15 +30,16 @@ func RunEtcdCommandFromWorker(deployment, workerID string, args ...string) strin
 	return ss
 }
 
-func RunEtcdCommandFromMasterWithFullPrivilege(deployment, masterID string, args ...string) string {
+func RunEtcdCommandFromMasterWithFullPrivilege(etcdVersion int, deployment, masterID string, args ...string) string {
 	// to test manually, this is the combined base command:
 	// /var/vcap/packages/etcdctl/etcdctl --endpoints https://master-0.etcd.cfcr.internal:2379 --cert /var/vcap/jobs/etcd/config/etcdctl.crt --key /var/vcap/jobs/etcd/config/etcdctl.key --cacert /var/vcap/jobs/etcd/config/etcdctl-ca.crt
 	remoteArgs := []string{
+		fmt.Sprintf("ETCDCTL_API=%d", etcdVersion),
 		"/var/vcap/packages/etcdctl/etcdctl",
 		fmt.Sprintf("--endpoints https://%s:2379", EtcdHostname),
-		"--cert", "/var/vcap/jobs/etcd/config/etcdctl.crt",
-		"--key", "/var/vcap/jobs/etcd/config/etcdctl.key",
-		"--cacert", "/var/vcap/jobs/etcd/config/etcdctl-ca.crt",
+		getCertFlag(etcdVersion), "/var/vcap/jobs/etcd/config/etcdctl.crt",
+		getKeyFlag(etcdVersion), "/var/vcap/jobs/etcd/config/etcdctl.key",
+		getCacertFlag(etcdVersion), "/var/vcap/jobs/etcd/config/etcdctl-ca.crt",
 	}
 	remoteArgs = append(remoteArgs, args...)
 	s := RunSSHWithDeployment(deployment, "master/"+masterID, fmt.Sprintf("sudo su -c '%s'", strings.Join(remoteArgs, " ")))
@@ -61,4 +63,28 @@ func RunCommand(cmd string, args ...string) *gexec.Session {
 	Expect(err).NotTo(HaveOccurred())
 
 	return session
+}
+
+func getCertFlag(etcdVersion int) string {
+	if etcdVersion == 2 {
+		return "--cert-file"
+	} else {
+		return "--cert"
+	}
+}
+
+func getKeyFlag(etcdVersion int) string {
+	if etcdVersion == 2 {
+		return "--key-file"
+	} else {
+		return "--key"
+	}
+}
+
+func getCacertFlag(etcdVersion int) string {
+	if etcdVersion == 2 {
+		return "--ca-file"
+	} else {
+		return "--cacert"
+	}
 }
