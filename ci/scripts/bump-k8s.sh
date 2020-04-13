2ccbdc01347eda7e4eee604f3b8398be78c9d888
Larry Hamel <lhamel@pivotal.io>
2020-04-13 14:50:34 -0700

    Refactor generate-pr and bump-k8s scripts
    Co-authored-by: Larry Hamel <lhamel@pivotal.io>
    Co-authored-by: Daniel Keeney <dkeeney@pivotal.io>

--- ci/scripts/bump-k8s.sh
+++ ci/scripts/bump-k8s.sh
@@ -3,18 +3,20 @@ set -exu -o pipefail
 
 source git-pks-kubo-release-ci/ci/scripts/lib/generate-pr.sh
 
-pr_release() {
+download_and_add_blob_and_commit() {
+  local version git_repo script_name base_branch component
   version="$1"
   git_repo="$2"
   script_name="$3"
   base_branch="$4"
+  component="$5"
 
+  # this is tightly coupled with the name of the resources declared in the pipeline yml
   concourse_base_name="git-${git_repo}"
 
-  cp -r "${concourse_base_name}/." "${concourse_base_name}-output"
-  pushd "${concourse_base_name}-output"
+  pushd "${concourse_base_name}"
 
-  ../git-pks-kubo-release-ci/ci/scripts/$script_name $version $(pwd)
+  ../git-pks-kubo-release-ci/ci/scripts/$script_name $version "$(pwd)"
 
   if [ -n "$(git status --porcelain)" ]; then
     cat <<EOF > "config/private.yml"
@@ -26,31 +28,58 @@ blobstore:
 $GCS_JSON_KEY
 EOF
     bosh upload-blobs
-    generate_pull_request "kubernetes" "$version" "${git_repo}" "${base_branch}"
+    commit "$component" "$version"
   else
-    echo "Kubernetes version is already up-to-date"
+    echo "Component version is already up-to-date"
   fi
 
   popd
 }
 
+determine_docker_version() {
+  local kubernetes_version minor_docker_version docker_version
+  kubernetes_version="$1"
+
+  curl -o k8s-dependencies.yml https://raw.githubusercontent.com/kubernetes/kubernetes/v${kubernetes_version}/build/dependencies.yaml
+  minor_docker_version=$(cat k8s-dependencies.yml | yq '.dependencies[] | select(.name == "docker") | .version')
+  curl -o DockerMsftIndex.json https://dockermsft.azureedge.net/dockercontainer/DockerMsftIndex.json
+  docker_version=$(cat DockerMsftIndex.json | jq ".channels[\"${minor_docker_version}\"].version" -r)
+
+  echo "$docker_version"
+}
+
 main() {
+  local git_repo k8s_script_name docker_script_name k8s_version docker_version
 
   if [ "${REPO:-}" == "windows" ]; then
     git_repo="pks-kubo-release-windows"
-    script_name="download_k8s_binaries_windows"
+    k8s_script_name="download_k8s_binaries_windows"
+    docker_script_name="download_docker_binaries_windows.sh"
   else
     git_repo="pks-kubo-release"
+    docker_script_name="download_docker_binaries_linux.sh"
     if [ "false" == "$USE_COMMON_CORE" ]; then
-      script_name="download_k8s_binaries_google"
+      k8s_script_name="download_k8s_binaries_google"
     else
-      script_name="download_k8s_binaries_common_core"
+      k8s_script_name="download_k8s_binaries_common_core"
     fi
   fi
 
   # BINARY_DIRECTORY should be declared in the pipeline via params
-  version=$(cat "$PWD/$BINARY_DIRECTORY/version")
-  pr_release "$version" "$git_repo" "$script_name" "${BASE_BRANCH}"
+  k8s_version=$(cat "$PWD/$BINARY_DIRECTORY/version")
+  docker_version=$(determine_docker_version "$k8s_version")
+
+  create_branch "kubernetes" "$k8s_version"
+  setup_git_config
+
+  download_and_add_blob_and_commit "$k8s_version" "$git_repo" "$k8s_script_name" "${BASE_BRANCH}" "kubernetes"
+  if [[ "$BUMP_DOCKER" == "true" ]]; then
+    download_and_add_blob_and_commit "$docker_version" "$git_repo" "$docker_script_name" "${BASE_BRANCH}" "docker"
+  fi
+
+  setup_ssh
+  push_current_branch
+  create_pr_through_curl "kubernetes" "$k8s_version" "${base_branch}" "${git_repo}"
 }
 
 main $@
--- ci/scripts/lib/generate-pr.sh
+++ ci/scripts/lib/generate-pr.sh
@@ -14,8 +14,12 @@ get_branch_name() {
 }
 
 create_branch() {
-  local branch_name=$1
-  git checkout -b $branch_name
+  local component tag
+  component="$1"
+  tag="$2"
+
+  branch_name=$(get_branch_name "$component" "$tag")
+  git checkout -b "$branch_name"
 }
 
 git_current_branch () {
@@ -27,14 +31,19 @@ git_current_branch () {
 		[[ $ret == 128 ]] && return
 		ref=$(command git rev-parse --short HEAD 2> /dev/null)  || return
 	fi
-	echo ${ref#refs/heads/}
+	echo "${ref#refs/heads/}"
 }
 
-commit_and_push() {
-  local component tag branch_name
-  component=$1
-  tag=$2
+commit() {
+  local component tag
+  component="$1"
+  tag="$2"
 
+  git add .
+  git commit -m "Bump $component to $tag"
+}
+
+setup_ssh() {
   mkdir -p ~/.ssh
   cat > ~/.ssh/config <<EOF
 StrictHostKeyChecking no
@@ -46,36 +55,24 @@ EOF
 ${GIT_SSH_KEY}
 EOF
   chmod 0600 ~/.ssh/id_rsa
-  eval $(ssh-agent) >/dev/null 2>&1
+  eval "$(ssh-agent)" >/dev/null 2>&1
   trap "kill $SSH_AGENT_PID" 0
   ssh-add ~/.ssh/id_rsa
-
-  git add .
-  git commit -m "Bump $component to $tag"
-  git push origin $(git_current_branch)
 }
 
-push_to_current_branch() {
-  local component=$1
-  local tag=$2
-
-  setup_git_config
-  commit_and_push "$component" "$tag"
+push_current_branch() {
+  git push origin "$(git_current_branch)"
 }
 
-# Needs to be called from the directory where PR needs to be generated
-generate_pull_request() {
-  local component=$1
-  local tag=$2
-  local repo=$3
-  local base_branch=$4
+create_pr_through_curl() {
+  local component tag branch_name base_branch repo
+  component="$1"
+  tag="$2"
+  base_branch="$3"
+  repo="$4"
 
-  setup_git_config
   branch_name=$(get_branch_name "$component" "$tag")
-  create_branch $branch_name
-  commit_and_push "$component" "$tag"
 
-  # create a PR here
   payload=$(create_pr_payload "$component" "$tag" "$branch_name" "$base_branch")
   curl -u "pks-bosh-lifecycle:${GIT_USER_TOKEN}" -H "Content-Type: application/json" -X POST -d "$payload" "https://api.github.com/repos/pivotal-cf/${repo}/pulls" --fail
 }
