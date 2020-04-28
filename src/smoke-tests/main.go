be97e19bf5402831e86383f0924fe27fce031007
Todd Sedano <tsedano@pivotal.io>
2020-04-28 11:04:45 -0700

    Adds simple server
    [#168887539]
    
    Co-authored-by: Todd Sedano <tsedano@pivotal.io>
    Co-authored-by: Sam Mirza <smirza@pivotal.io>

---
+++ src/smoke-tests/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+
+	fmt.Fprintf(w, "Server: simple server\n")
+}
+
+func main() {
+
+	http.HandleFunc("/", hello)
+
+	http.ListenAndServe(":80", nil)
+}
