9da62ad3a5a2083de61f8b93330f3a4f20c58085
Bridget Arthur <barthur@pivotal.io>
2020-05-14 14:47:04 -0600

    Remove git status check

--- ci/tasks/config-blobstore/task.sh
+++ ci/tasks/config-blobstore/task.sh
@@ -3,26 +3,22 @@ set -exu -o pipefail
 
 pushd "git-pks-kubernetes-release"
 
-  if [ -n "$(git status --porcelain)" ]; then
-    cat <<EOF > "config/private.yml"
+cat <<EOF > "config/private.yml"
 blobstore:
   options:
     credentials_source: static
     json_key: |
 $GCS_JSON_KEY
 EOF
-  fi
 popd
 
 pushd "git-pks-kubernetes-windows-release"
 
-  if [ -n "$(git status --porcelain)" ]; then
-    cat <<EOF > "config/private.yml"
+cat <<EOF > "config/private.yml"
 blobstore:
   options:
     credentials_source: static
     json_key: |
 $GCS_JSON_KEY
 EOF
-  fi
 popd
