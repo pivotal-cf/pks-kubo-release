4d87623285d2cb85a00447db63f0a93861b1d87c
Bridget Arthur <barthur@pivotal.io>
2020-05-14 15:42:19 -0600

    Modify for final release deployments

--- ci/open-source-management/deploy-manifest.sh
+++ ci/open-source-management/deploy-manifest.sh
@@ -2,11 +2,24 @@
 
 set -euxo pipefail
 
-source git-boshcycle-ci/ci/utils/use-bosh.sh
-source git-boshcycle-ci/ci/open-source-management/deployment-name.sh
+source git-pks-kubernetes-release-ci/ci/utils/use-bosh.sh
+source git-pks-kubernetes-release-ci/ci/open-source-management/deployment-name.sh
 
-PKS_KUBO_VERSION=$(cat gcs-pks-kubo-release/version)
-PKS_KUBO_WINDOWS_VERSION=$(cat gcs-pks-kubo-release-windows/version)
+if [-d gcs-pks-kubo-release] #use tarball
+then
+  PKS_KUBO_VERSION=$(cat gcs-pks-kubo-release/version)
+else #use final release
+  files=(`ls git-pks-kubernetes-release/releases/kubo/*${VERSION}*`)
+  PKS_KUBO_VERSION=$(yq -r .version ${files[-1]})
+fi
+
+if [-d gcs-pks-kubo-release-windows] #use tarball
+then
+  PKS_KUBO_WINDOWS_VERSION=$(cat gcs-pks-kubo-release-windows/version)
+else #use final release
+  files=(`ls git-pks-kubernetes-windows-release/releases/kubo/*${VERSION}*`)
+  PKS_KUBO_WINDOWS_VERSION=$(yq -r .version ${files[-1]})
+fi
 
 export NETWORK_NAME=`bosh int <(bosh cloud-config) --path /networks | yq -r '.[] | select(.name|test(".*pks-services-subnet.*")) | .name'`
 export FIRST_AZ=`bosh int <(bosh cloud-config) --path /networks | yq -r '.[] | select(.name|test(".*pks-services-subnet.*")) | .subnets[0].azs[0]'`
@@ -19,4 +32,4 @@ bosh deploy \
   --var=kubo-windows-version="${PKS_KUBO_WINDOWS_VERSION}" \
   --var=network-name="${NETWORK_NAME}" \
   --var=first-az="${FIRST_AZ}" \
-  git-boshcycle-ci/ci/open-source-management/osm-deployment-manifest.yml
+git-boshcycle-ci/ci/open-source-management/deploy-manifest.yml
