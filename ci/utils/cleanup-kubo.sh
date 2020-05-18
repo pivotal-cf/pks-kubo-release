9d61096282617108f74ad55f7bf961d2a3fbc5d6
Todd Sedano <tsedano@pivotal.io>
2020-05-18 16:53:20 -0700

    Osl pipeline (#65)
    Add new OSL pipeline
    * Outputs information needed for OSL
    
    [#171660758]
    [#171820094]
    
    Co-authored-by: Shimon Walner <swalner@pivotal.io>
    Co-authored-by: Bridget Arthur <barthur@pivotal.io>
    Co-authored-by: Todd Sedano <tsedano@pivotal.io>
    Co-authored-by: Daniel Keeney <dkeeney@pivotal.io>

---
+++ ci/utils/cleanup-kubo.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+source git-pks-kubernetes-release-ci/ci/utils/use-bosh.sh
+
+cleanupKubo() {
+  local deployment=$1
+
+  if [ -d "$1" ]
+  then
+    bosh delete-deployment \
+      --non-interactive \
+      --deployment="${deployment}" \
+      || true
+  fi
+}
+
+cleanupKuboWindows() {
+  local deployment=$1
+
+  if [ -d "$1" ]
+  then
+    bosh delete-deployment \
+      --non-interactive \
+      --deployment="${deployment}" \
+      || true
+  fi
+}
+
