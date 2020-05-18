757be36f8df93d92f984ba45a6608b2a3f6078ba
Bridget Arthur <barthur@pivotal.io>
2020-05-18 12:43:05 -0600

    Fix regex for tarball name

--- ci/utils/upload-kubo-release-windows.sh
+++ ci/utils/upload-kubo-release-windows.sh
@@ -6,6 +6,6 @@ set -euxo pipefail
 source git-pks-kubernetes-release-ci/ci/utils/use-bosh.sh
 
 pushd pks-kubernetes-windows-release
-  RELEASE_TARBALL=$(ls kubo-release-*.tgz)
+  RELEASE_TARBALL=$(ls kubo-*.tgz)
   bosh upload-release ${RELEASE_TARBALL}
 popd
