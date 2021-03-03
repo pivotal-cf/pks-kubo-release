3a91c1a3ec4751cb848151ada553bc8f28864724
hongtaiwang <wanghongtai042@gmail.com>
2021-03-03 20:34:53 +0800

    fixed unit test
    (cherry picked from commit 482ec01fb44b5e4c2d76e9916fac10d2e966ed22)

--- spec/kubelet_drain_spec.rb
+++ spec/kubelet_drain_spec.rb
@@ -18,13 +18,13 @@ describe 'kubelet drain' do
       }
     }
 
-    rendered_drain = compiled_template('kubelet', 'bin/drain', manifest_properties, {}, {}, nil, nil, nil)
+    rendered_drain = compiled_template('kubelet', 'bin/pre-stop', manifest_properties, {}, {}, nil, nil, nil)
     k8s_disks_dir = get_k8s_disks_dir(rendered_drain)
     expect(k8s_disks_dir).to eq(manifest_properties['k8s-args']['root-dir'].gsub("/", "\\/"))
   end
 
   it 'uses /var/lib/kubelet for get_k8s_disks() by default' do
-    rendered_drain = compiled_template('kubelet', 'bin/drain', {}, {}, {}, nil, nil, nil)
+    rendered_drain = compiled_template('kubelet', 'bin/pre-stop', {}, {}, {}, nil, nil, nil)
     k8s_disks_dir = get_k8s_disks_dir(rendered_drain)
     expect(k8s_disks_dir).to eq("/var/lib/kubelet".gsub("/", "\\/"))
   end
